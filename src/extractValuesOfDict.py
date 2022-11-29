73603dd6cda093c2e63a66c4ea4a40f265f6a0f1
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-11-30 00:15:34 +0100

    Refactor function for extracting dict values to list comprehension

--- src/extractValuesOfDict.py
+++ src/extractValuesOfDict.py
@@ -3,11 +3,4 @@ from src.extractString import stripString
 
 def extractDictValues(stringCPE):
 
-    arrayForValuesToDict = []
-
-    for i in stripString(stringCPE).split(":"):
-        if i == "*":
-            arrayForValuesToDict.append("ANY")
-            continue
-        arrayForValuesToDict.append(i)
-    return arrayForValuesToDict
+    return ["ANY" if i == "*" else i for i in stripString(stringCPE).split(":")]
