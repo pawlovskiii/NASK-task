3a328ef299713a1d77460ed47d279a9a9e5fe801
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-11-29 23:27:18 +0100

    Create a separate folder for additional Python modules

---
+++ src/data.py
@@ -0,0 +1,15 @@
+listOfKeysOfDict = [
+    "part",
+    "vendor",
+    "product",
+    "version",
+    "update",
+    "edition",
+    "language",
+    "sw_edition",
+    "target_sw",
+    "target_hw",
+    "other",
+]
+
+stringCPE = "cpe:2.3:a:microsoft:internet_explorer:8.0.6001:beta:*:*:*:*:*:*"
