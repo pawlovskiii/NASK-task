da4cb4a730df433d799cb7a1bc7bbe5e649d5c64
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-11-29 23:22:05 +0100

    Create an initial version of the working parser

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-def bind_to_fs(w):
-    return w
+def stripString(stringCPE):
+    return stringCPE.strip("cpe:2.3")
 
 
-def bind_value_for_fs(v):
-    return v
+def extractDictValues(stringCPE):
+
+    arrayForValuesToDict = []
+
+    for i in stripString(stringCPE).split(":"):
+        if i == "*":
+            arrayForValuesToDict.append("ANY")
+            continue
+        arrayForValuesToDict.append(i)
+    return arrayForValuesToDict
 
 
-def process_quoted_chars(s):
-    return s
+a = [
+    "part",
+    "vendor",
+    "product",
+    "version",
+    "update",
+    "edition",
+    "language",
+    "sw_edition",
+    "target_sw",
+    "target_hw",
+    "other",
+]
 
 
-# cpe:2.3:a:microsoft:internet_explorer:8.0.6001:beta:*:*:*:*:*:*
+def mergeArraysToDict(keys, values):
+    return dict(zip(keys, values))
+
+
+print(mergeArraysToDict(a, extractDictValues("cpe:2.3:a:microsoft:internet_explorer:8.0.6001:beta:*:*:*:*:*:*")))
