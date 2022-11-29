6e05e5e4b87aa1c2e2042ec3d40f23d2d9d9a243
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-11-29 22:25:53 +0100

    Prepare functions from the documentation to rewrite the pseudocode

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def parserCPE(argsCPE):
-    return argsCPE
+def bind_to_fs(w):
+    return w
 
 
-print(parserCPE('cpe:2.3:a:microsoft:internet_explorer:8.0.6001:beta:*:*:*:*:*:*'))
+def bind_value_for_fs(v):
+    return v
+
+
+def process_quoted_chars(s):
+    return s
+
+
+# cpe:2.3:a:microsoft:internet_explorer:8.0.6001:beta:*:*:*:*:*:*
