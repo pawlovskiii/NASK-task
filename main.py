eeec2e9e1f258194602000d6e89aecdd201a41c1
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-11-29 23:29:54 +0100

    Import variables from data module

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from src.data import listOfKeysOfDict, stringCPE
+
+
 def stripString(stringCPE):
     return stringCPE.strip("cpe:2.3")
 
@@ -14,23 +17,12 @@ def extractDictValues(stringCPE):
     return arrayForValuesToDict
 
 
-a = [
-    "part",
-    "vendor",
-    "product",
-    "version",
-    "update",
-    "edition",
-    "language",
-    "sw_edition",
-    "target_sw",
-    "target_hw",
-    "other",
-]
-
-
 def mergeArraysToDict(keys, values):
     return dict(zip(keys, values))
 
 
-print(mergeArraysToDict(a, extractDictValues("cpe:2.3:a:microsoft:internet_explorer:8.0.6001:beta:*:*:*:*:*:*")))
+print(
+    mergeArraysToDict(
+        listOfKeysOfDict, extractDictValues(stringCPE)
+    )
+)
