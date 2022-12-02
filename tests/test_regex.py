b3f943fe4ecd301764dce133353f5074f413e90f
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-12-02 21:06:29 +0100

    Add a unit test for checking the regex expression validation

---
+++ tests/test_regex.py
@@ -0,0 +1,21 @@
+import pytest
+import sys
+
+# setting path
+sys.path.append("../src")
+from validationOfStringCPE import regExValidationOfStringCPE
+
+
+def test_input():
+
+    listOfPossibleInputs = [
+        "a:microsoftinternet_explorer:8.0.6001:beta:*:*:*:*:*:*",
+        "a:microsoft:internet_explorer:high:beta:*:*:*:*:*:*",
+        "a:microsoft:internet_explorer:8.0.6001:55:*:*:*:*:*:*",
+        "23:microsoft:internet_explorer:8.0.6001:beta:*:*:*:*:*:*",
+        "a:123:internet_explorer:8.0.6001:beta:*:*:*:*:*:*"
+    ]
+
+    for item in listOfPossibleInputs:
+        with pytest.raises(ValueError):
+            regExValidationOfStringCPE(item)
