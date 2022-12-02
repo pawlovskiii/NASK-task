9d902c6b112ec215a7a84eaaf50a38e1468340bf
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-12-02 20:50:27 +0100

    Add unit test for the testing type of input

---
+++ tests/test_type_Of_input.py
@@ -0,0 +1,15 @@
+import pytest
+import sys
+
+# setting path
+sys.path.append("../src")
+from validationOfStringCPE import validationOfInputType
+
+
+def test_input_type():
+
+    listOfPossibleInputs = [123, ["check", 56], 6.5, {"car": "bmw"}, (3, 5)]
+
+    for item in listOfPossibleInputs:
+        with pytest.raises(TypeError):
+            validationOfInputType(item)
