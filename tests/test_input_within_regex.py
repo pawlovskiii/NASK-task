9262c1e9692d9e0e94012b60ee144b3c092b6084
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-12-04 23:47:54 +0100

    Correct the tests within the regex validation

--- tests/test_input_within_regex.py
+++ tests/test_input_within_regex.py
@@ -9,11 +9,9 @@ from validationOfStringCPE import regExValidationOfStringCPE
 def test_input_within_regex():
 
     listOfPossibleInputs = [
-        "a:microsoftinternet_explorer:8.0.6001:beta:*:*:*:*:*:*",
-        "a:microsoft:internet_explorer:high:beta:*:*:*:*:*:*",
-        "a:microsoft:internet_explorer:8.0.6001:55:*:*:*:*:*:*",
-        "23:microsoft:internet_explorer:8.0.6001:beta:*:*:*:*:*:*",
-        "a:123:internet_explorer:8.0.6001:beta:*:*:*:*:*:*"
+        "amicrosoft:internet_explorer:8.0.6001:beta:*:*:*:*:*:*",
+        "amicrosoft:internet_explorer8.0.6001beta:*:*:*:*:*:*",
+        "amicrosoftinternet_explorer8.0.6001beta******"
     ]
 
     for item in listOfPossibleInputs:
