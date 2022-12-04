be4c6b71b5c06e2c28cd8dd8a2a1fd358a6c6611
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-12-05 00:53:26 +0100

    Add more detailed descriptions to the error messages

--- src/validationOfStringCPE.py
+++ src/validationOfStringCPE.py
@@ -3,24 +3,30 @@ import re
 
 def checkSpaceInStringCPE(takeInputFromUserOrUseSampleStringCPE):
     if " " in takeInputFromUserOrUseSampleStringCPE:
-        raise ValueError("String CPE 2.3 cannot include whitespace. Please Try again.")
+        raise ValueError("String CPE 2.3 cannot include whitespace. \nExample of valid input: cpe:2.3:a:microsoft:internet_explorer:8.0.6001:beta:*:*:*:*:*:*")
     return takeInputFromUserOrUseSampleStringCPE
 
 
 def validationOfInputType(takeInputFromUserOrUseSampleStringCPE: str) -> str:
     if not isinstance(takeInputFromUserOrUseSampleStringCPE, str):
-        raise TypeError("Wrong type! Input must be a string.")
+        raise TypeError(
+            "Wrong type! Input must be a string.\nExample of valid input: cpe:2.3:a:microsoft:internet_explorer:8.0.6001:beta:*:*:*:*:*:*"
+        )
     return takeInputFromUserOrUseSampleStringCPE
 
 
 def validationOfTheBeginningOfTheString(takeInputFromUserOrUseSampleStringCPE: str) -> str:
     if not takeInputFromUserOrUseSampleStringCPE.startswith("cpe:2.3"):
-        raise ValueError("Incorrect beginning of a string CPE 2.3 It must starts with 'cpe:2.3'")
+        raise ValueError(
+            "Incorrect beginning of a string CPE 2.3 It must starts with 'cpe:2.3'\nExample of valid input: cpe:2.3:a:microsoft:internet_explorer:8.0.6001:beta:*:*:*:*:*:*"
+        )
     return takeInputFromUserOrUseSampleStringCPE
 
 
 def regExValidationOfStringCPE(strippedStringCPE: str) -> str:
     regex = '^[aoh]:[a-zA-Z0-9@_!#$%^&*()<>?\/|}{~:;,.`"=\\]+:[a-zA-Z0-9@_!#$%^&*()<>?\/|}{~:;,.`"=\\]+:[a-zA-Z0-9@_!#$%^&*()<>?\/|}{~:;,.`"=\\]+:[a-zA-Z0-9@_!#$%^&*()<>?\/|}{~:;,.`"=\\]+:[a-zA-Z0-9@_!#$%^&*()<>?\/|}{~:;,.`"=\\]+:[a-zA-Z*-]+:[a-zA-Z0-9@_!#$%^&*()<>?\/|}{~:;,.`"=\\]+:[a-zA-Z0-9@_!#$%^&*()<>?\/|}{~:;,.`"=\\]+:[a-zA-Z0-9@_!#$%^&*()<>?\/|}{~:;,.`"=\\]+:[a-zA-Z0-9@_!#$%^&*()<>?\/|}{~:;,.`"=\\]+$'
     if not re.search(regex, strippedStringCPE):
-        raise ValueError("Invalid string CPE 2.3")
+        raise ValueError(
+            "Invalid string CPE 2.3 \nExample of valid input: cpe:2.3:a:microsoft:internet_explorer:8.0.6001:beta:*:*:*:*:*:*"
+        )
     return strippedStringCPE
