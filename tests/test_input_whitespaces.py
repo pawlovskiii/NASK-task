1461caa5a760bee16b1f67c74aabf73dae1d8a6a
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-12-04 22:31:24 +0100

    Add test script file for checking if stringCPE includes whitespaces

---
+++ tests/test_input_whitespaces.py
@@ -0,0 +1,21 @@
+import pytest
+import sys
+
+# setting path
+sys.path.append("../src")
+from validationOfStringCPE import regExValidationOfStringCPE
+
+
+def test_input_within_regex():
+
+    listOfPossibleInputs = [
+        " a:microsoftinternet_explorer:8.0.6001:beta:*:*:*:*:*:*",
+        "a:microsoftinternet_explorer:8.0.6001:beta:*:*:*:*:*:* ",
+        "a:microsoftinternet _ explorer:8.0.6001:beta:*:*:*:*:*:*",
+        "a : microsoftinternet_explorer : 8.0.6001 : beta:*:*:*:*:*:*",
+        "a:microsoftinternet_explorer:8.0.6001:beta: * : * : * : * : * : *"
+    ]
+
+    for item in listOfPossibleInputs:
+        with pytest.raises(ValueError):
+            regExValidationOfStringCPE(item)
