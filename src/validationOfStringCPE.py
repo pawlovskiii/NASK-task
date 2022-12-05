75219474cb52196ccd0e8c8b7f072b9d9e3a4a9b
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-12-05 02:45:29 +0100

    Add test cases to correct the regular expression

--- src/validationOfStringCPE.py
+++ src/validationOfStringCPE.py
@@ -3,7 +3,9 @@ import re
 
 def checkSpaceInStringCPE(takeInputFromUserOrUseSampleStringCPE):
     if " " in takeInputFromUserOrUseSampleStringCPE:
-        raise ValueError("String CPE 2.3 cannot include whitespace. \nExample of valid input: cpe:2.3:a:microsoft:internet_explorer:8.0.6001:beta:*:*:*:*:*:*")
+        raise ValueError(
+            "String CPE 2.3 cannot include whitespace. \nExample of valid input: cpe:2.3:a:microsoft:internet_explorer:8.0.6001:beta:*:*:*:*:*:*"
+        )
     return takeInputFromUserOrUseSampleStringCPE
 
 
@@ -24,8 +26,8 @@ def validationOfTheBeginningOfTheString(takeInputFromUserOrUseSampleStringCPE: s
 
 
 def regExValidationOfStringCPE(strippedStringCPE: str) -> str:
-    regex = '^[aoh]:[a-zA-Z0-9@_!#$%^&*()<>?\/|}{~:;,.`"=\\]+:[a-zA-Z0-9@_!#$%^&*()<>?\/|}{~:;,.`"=\\]+:[a-zA-Z0-9@_!#$%^&*()<>?\/|}{~:;,.`"=\\]+:[a-zA-Z0-9@_!#$%^&*()<>?\/|}{~:;,.`"=\\]+:[a-zA-Z0-9@_!#$%^&*()<>?\/|}{~:;,.`"=\\]+:[a-zA-Z*-]+:[a-zA-Z0-9@_!#$%^&*()<>?\/|}{~:;,.`"=\\]+:[a-zA-Z0-9@_!#$%^&*()<>?\/|}{~:;,.`"=\\]+:[a-zA-Z0-9@_!#$%^&*()<>?\/|}{~:;,.`"=\\]+:[a-zA-Z0-9@_!#$%^&*()<>?\/|}{~:;,.`"=\\]+$'
-    if not re.search(regex, strippedStringCPE):
+    regex = '^[aoh]:[a-zA-Z0-9@_!#$%^&*()<>?/|}{~:;,.`"=-\\\\-\\]+:[a-zA-Z0-9@_!#$%^&*()<>?/|}{~:;,.`"=-\\\\-\\-]+:[a-zA-Z0-9@_!#$%^&*()<>?/|}{~:;,.`"=-\\\\-\\-]+:[a-zA-Z0-9@_!#$%^&*()<>?/|}{~:;,.`"=-\\\\-\\-]+:[a-zA-Z0-9@_!#$%^&*()<>?/|}{~:;,.`"=-\\\\-\\-]+:[a-zA-Z*-]+:[a-zA-Z0-9@_!#$%^&*()<>?/|}{~:;,.`"=-\\\\-\\-]+:[a-zA-Z0-9@_!#$%^&*()<>?/|}{~:;,.`"=-\\\\-\\-]+:[a-zA-Z0-9@_!#$%^&*()<>?/|}{~:;,.`"=-\\\\-\\-]+:[a-zA-Z0-9@_!#$%^&*()<>?/|}{~:;,.`"=-\\\\-\\-]+$'
+    if not re.search(regex, r'{}'.format(strippedStringCPE)):
         raise ValueError(
             "Invalid string CPE 2.3 \nExample of valid input: cpe:2.3:a:microsoft:internet_explorer:8.0.6001:beta:*:*:*:*:*:*"
         )
--- tests/test_input_within_regex.py
+++ tests/test_input_within_regex.py
@@ -6,14 +6,29 @@ sys.path.append("../src")
 from validationOfStringCPE import regExValidationOfStringCPE
 
 
-def test_input_within_regex():
+def test_input_within_regex_error():
 
     listOfPossibleInputs = [
         "amicrosoft:internet_explorer:8.0.6001:beta:*:*:*:*:*:*",
-        "amicrosoft:internet_explorer8.0.6001beta:*:*:*:*:*:*",
-        "amicrosoftinternet_explorer8.0.6001beta******"
+        "amicrosoftinternet_explorer8.0.6001beta******",
+        "a:microsoft:",
     ]
 
     for item in listOfPossibleInputs:
         with pytest.raises(ValueError):
             regExValidationOfStringCPE(item)
+
+
+def test_input_within_regex_assert():
+
+    listOfPossible = [
+        "a:microsoft:internet_explorer:8.0.6001:foo-bar:*:*:*:*:*:*",
+        "a:back\\-=slash_software:internet_explorer:8.0.6001:beta:*:*:*:*:*:*",
+        "a:foo\-bar:internet_explorer:8.0.6001:beta:*:*:*:*:*:*",
+        "a:*8\.??:internet_explorer:8.0.6001:beta:*:*:*:*:*:*",
+        "a:*8\.??:foo\:bar:foo\:bar:foo\:bar:foo\:bar:foo\:bar:foo\:bar:foo\:bar:foo\:bar:foo\:bar",
+        "a:*8\\.??:foo\\:bar:foo\\:bar:foo\\:bar:foo\\:bar:foo\\:bar:foo\\:bar:foo\\:bar:foo\\:bar:foo\\:bar",
+    ]
+
+    for item in listOfPossible:
+        assert regExValidationOfStringCPE(item) == item
