c54a98ec8d9382cb3a0af2822c01f7c637acf760
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-12-05 02:50:41 +0100

    Refactor format using f string

--- src/validationOfStringCPE.py
+++ src/validationOfStringCPE.py
@@ -27,7 +27,7 @@ def validationOfTheBeginningOfTheString(takeInputFromUserOrUseSampleStringCPE: s
 
 def regExValidationOfStringCPE(strippedStringCPE: str) -> str:
     regex = '^[aoh]:[a-zA-Z0-9@_!#$%^&*()<>?/|}{~:;,.`"=-\\\\-\\]+:[a-zA-Z0-9@_!#$%^&*()<>?/|}{~:;,.`"=-\\\\-\\-]+:[a-zA-Z0-9@_!#$%^&*()<>?/|}{~:;,.`"=-\\\\-\\-]+:[a-zA-Z0-9@_!#$%^&*()<>?/|}{~:;,.`"=-\\\\-\\-]+:[a-zA-Z0-9@_!#$%^&*()<>?/|}{~:;,.`"=-\\\\-\\-]+:[a-zA-Z*-]+:[a-zA-Z0-9@_!#$%^&*()<>?/|}{~:;,.`"=-\\\\-\\-]+:[a-zA-Z0-9@_!#$%^&*()<>?/|}{~:;,.`"=-\\\\-\\-]+:[a-zA-Z0-9@_!#$%^&*()<>?/|}{~:;,.`"=-\\\\-\\-]+:[a-zA-Z0-9@_!#$%^&*()<>?/|}{~:;,.`"=-\\\\-\\-]+$'
-    if not re.search(regex, r'{}'.format(strippedStringCPE)):
+    if not re.search(regex, fr"{strippedStringCPE}"):
         raise ValueError(
             "Invalid string CPE 2.3 \nExample of valid input: cpe:2.3:a:microsoft:internet_explorer:8.0.6001:beta:*:*:*:*:*:*"
         )
